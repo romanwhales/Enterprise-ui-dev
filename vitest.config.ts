3fdb1de6fbda7da7562e85b36dc57ce9c950e9de
Olawale Adedeji <oadedej@LLF2T9PXGWCP.ngco.com>
2023-07-11 12:26:50 -0400

    Update coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       ['**/*.component.test.ts', 'jsdom'],
     ],
     coverage: {
-      statements: 60.4,
+      statements: 58,
       thresholdAutoUpdate: true,
       include: ['src/**/*'],
       exclude: [
